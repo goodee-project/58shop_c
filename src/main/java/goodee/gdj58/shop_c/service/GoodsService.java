d678c4a376261c1f84e29918699f8f01cb8cc211
mis25 <mis2560@naver.com>
2023-03-03 16:38:29 +0900

    [김설]service,controller 생성

---
+++ src/main/java/goodee/gdj58/shop_c/service/GoodsService.java
@@ -0,0 +1,11 @@
+package goodee.gdj58.shop_c.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class GoodsService {
+	
+}
