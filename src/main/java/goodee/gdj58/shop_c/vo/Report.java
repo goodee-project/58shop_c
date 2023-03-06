34255d3a57886a98758d7fa3abb6e9fbda06a783
dllllllrkdms <mlee33444@gmail.com>
2023-03-06 11:13:55 +0900

    [이가은] vo.Report 필드 변경

--- src/main/java/goodee/gdj58/shop_c/vo/Report.java
+++ src/main/java/goodee/gdj58/shop_c/vo/Report.java
@@ -5,7 +5,8 @@ import lombok.Data;
 @Data
 public class Report { // 신고
 	private int reportNo;
-	private int orderSheetNo; // 주문서
-	private String reportContent; // 신고 내용
+	private String reportCategory; // '예약', '쇼핑'
+	private int reportContent; // 예약 or 주문 번호
+	private String reportMemo;
 	private String createdate;
 }
