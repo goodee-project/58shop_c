175d36a8620f45d22edf4a853965a16ff22c842d
DoubleTing0 <mg4476@gmail.com>
2023-03-24 17:38:37 +0900

    [진민구] 주문 취소 기능 구현(재고 처리 할 추가예정)

--- src/main/java/goodee/gdj58/shop_c/mapper/GoodsOrderMapper.java
+++ src/main/java/goodee/gdj58/shop_c/mapper/GoodsOrderMapper.java
@@ -12,35 +12,76 @@ import org.apache.ibatis.annotations.Mapper;
 public interface GoodsOrderMapper {
 	List<Map<String, Object>> selectOrdersList(Map<String, Object> paramMap);
 	
-	// 주문서 주문상태 취소로 변경
-	int cUpdateOrderSheetStateCancel(HashMap<String, Object> paramMap);
+	// ====================== 주문 취소 시작 ======================
 	
-	// 주문서 company_id, customer_address_no 저장
-	HashMap<String, Object> cSelectCompanyIdCustomerAddress(HashMap<String, Object> paramMap);
-	
-	// 주문 중 결제완료인 goods_option_no, quantity 저장
-	ArrayList<HashMap<String, Object>> cSelectOptionNoQuantityPrice(HashMap<String, Object> paramMap);
-	
-	// 해당 주문 주문상태 취소로 변경
-	int cUpdateOrderStateCancel(HashMap<String, Object> paramMap);
-	
-	// 사용 총 포인트가 0보다 크다면 point_save 이력 추가
-	int cInsertPointSaveHistory(HashMap<String, Object> paramMap);
-	
-	// 주문에 사용한 총 포인트
-	int cSelectSumPointOrderCancel(HashMap<String, Object> paramMap);
-	
-	// 주문서 가격(결제 가격) total_use_pay 복구
-	int cUpdateTotalUsePayRewind(HashMap<String, Object> paramMap);
-	
-	// 주문서 가격(결제 가격) pay_save 이력 추가
-	int cInsertPaySaveHistory(HashMap<String, Object> paramMap);
+	// 결제 완료 된 주문 count
+	int cSelectCountOrderPayment(HashMap<String, Object> paramMap);
 	
 	// cancel_history 주문 이력 추가
 	int cInsertCancelHistory(HashMap<String, Object> paramMap);
 	
-	// 결제 완료 된 주문 count
-	int cSelectCountOrderPayment(HashMap<String, Object> paramMap);
+	// 취소 전 결제 했었던 주문서 가격
+	int cSelectOrderSheetPrice(HashMap<String, Object> paramMap);
+	
+	// 주문서 가격(결제 가격) pay_save 이력 추가
+	int cInsertPaySaveHistory(HashMap<String, Object> paramMap);
+	
+	// 주문서 가격(결제 가격) total_use_pay 복구
+	int cUpdateTotalUsePayRewind(HashMap<String, Object> paramMap);
+	
+	// customer 테이블 pay 환급
+	int cUpdateCustomerPayRewind(HashMap<String, Object> paramMap);
+	
+	// 주문에 사용한 총 포인트
+	int cSelectSumPointOrderCancel(HashMap<String, Object> paramMap);
+	
+	// 사용 총 포인트가 0보다 크다면 point_save 이력 추가
+	int cInsertPointSaveHistory(HashMap<String, Object> paramMap);
+	
+	// 사용 총 포인트가 0보다 크다면 customer 테이블 point 환급
+	int cUpdateCustomerPointRewind(HashMap<String, Object> paramMap);
+	
+	// 해당 주문 주문상태 취소로 변경
+	int cUpdateOrderStateCancel(HashMap<String, Object> paramMap);
+	
+	// 주문 중 결제완료인 goods_option_no, quantity, price 저장
+	ArrayList<HashMap<String, Object>> cSelectOptionNoQuantityPrice(HashMap<String, Object> paramMap);
+	
+	// 주문서 company_id, customer_address_no 저장
+	HashMap<String, Object> cSelectCompanyIdCustomerAddress(HashMap<String, Object> paramMap);
+	
+	// 주문서 주문상태 취소로 변경
+	int cUpdateOrderSheetStateCancel(HashMap<String, Object> paramMap);
+	
+	// 나머지 모든 주문들 주문상태 취소로 변경
+	int cUpdateGoodsOrderStateCancel(HashMap<String, Object> paramMap);
+	
+	
+	
+	
+	// 새로운 주문서 생성 후 no 추출
+	int cInsertNewOrderSheet(HashMap<String, Object> newSheetMap);
+	
+	// 취소한 주문 제외하고 나머지 주문들 재구매(서비스에서 반복문 돌림)
+	int cInsertNewGoodsOrder(HashMap<String, Object> option);
+	
+	// 새로운 주문서 총 가격(등급 할인만 적용)
+	int	cSelectNewOrderSheetPrice(HashMap<String, Object> newSheetMap);
+	
+	// 새로운 주문서 가격 수정
+	int cUpdateNewOrderSheetPrice(HashMap<String, Object> newSheetMap);
+	
+	// pay_use_history 이력 추가
+	int cInsertPayUseHistory(HashMap<String, Object> newSheetMap);
+	
+	// customer 테이블 새 주문서 가격으로 pay 차감
+	int cUpdateCustomerPayBuy(HashMap<String, Object> newSheetMap);
+	
+	// total_use_pay 재구매 내용 적용
+	int cUpdateTotalUsePayBuy(HashMap<String, Object> newSheetMap);
+	
+	
+	// ====================== 주문 취소 끝 ======================
 	
 	
 	
--- src/main/java/goodee/gdj58/shop_c/service/GoodsOrderService.java
+++ src/main/java/goodee/gdj58/shop_c/service/GoodsOrderService.java
@@ -19,70 +19,70 @@ public class GoodsOrderService {
 	@Autowired
 	private GoodsOrderMapper goodsOrderMapper;
 	
-	// 주문 취소
 	/*
 		주문 취소
-		-> 페이, 포인트 환급 후 주문서(sheet) 취소
-		-> 취소한 항목 제외하고 다시 구매
+		-> 페이, 포인트 환급 후 주문서(sheet), 주문 취소
+		-> 취소한 항목 제외하고 새 주문서, 새 주문 생성
+		
+		===> 재고 관련 적용 해야함
 		
 		=================================================================
 		
-		count > 1
 		
-		주문 전체 취소
+		[주문 취소 과정]
 		
-		
-		cancel_history 주문 이력 추가
-		
-		주문서 가격(결제 가격) pay_save 이력 추가
-		
-		주문서 가격(결제 가격) total_use_pay 복구
-		
-		랭크 변한다면 수정 -> 스케줄러?
-		
-		주문들 포인트 sum이 0보다 크다면 point_save 이력 추가
-		
-		해당 주문 주문상태 취소 변경
-		
-		주문 중 결제완료인 goods_option_no, quantity 저장
-		
-		주문서 company_id, customer_address_no 저장
-		
-		주문서 주문서상태 취소 변경
-		
-		
-		
-		
-		새로 주문
-		
-		
-		주문서 생성
-		
-		주문 생성
-		
-		주문서 가격 수정(등급 할인 적용)(포인트는 미적용)
-		
-		pay_use 이력 추가
-		
-		total_use_pay 결제 내용 추가
+			cancel_history 주문 이력 추가
+			
+			취소 전 결제 했었던 주문서 가격
+			
+			주문서 가격(결제 가격) pay_save 이력 추가
+			
+			주문서 가격(결제 가격) total_use_pay 복구
+			
+			customer 테이블 pay 환급 
+			
+			주문들 포인트 sum이 0보다 크다면 
+			point_save 이력 추가
+			customer 테이블 point 환급
 		
 		=================================================================
 		
-		count = 1 이면 (삭제후 재구매 안함)
+			if(count > 1)	count : 주문 취소하기 전 결제 완료 상태인 주문 갯수
+			
+				해당 주문 주문상태 취소 변경
+				
+				주문 중 결제완료인 goods_option_no, quantity, price 저장
+				
+				주문서 company_id, customer_address_no 저장
+				
+				주문서 주문서상태 취소 변경
+				
+				나머지 모든 주문들 주문상태 취소로 변경
+				
+				
+				
+				
+				[취소 항목 제외하고 재구매 과정]
+				
+				새 주문서 생성
+				
+				새 주문 생성
+				
+				새 주문서 가격 수정(등급 할인 적용)(포인트는 미적용)
+				
+				pay_use_history 이력 추가
+				
+				customer 테이블 새 주문서 가격으로 pay 차감
+				
+				total_use_pay 재구매 내용 적용
 		
-		cancel_history 주문 이력 추가
+		=================================================================
 		
-		주문서 가격(결제 가격) pay_save 이력 추가
+			else if(count = 1) (삭제후 재구매 안함)
 		
-		주문서 가격(결제 가격) total_use_pay 복구
-		
-		랭크 변한다면 수정 -> 스케줄러?
-		
-		주문들 포인트 sum이 0보다 크다면 point_save 이력 추가
-		
-		해당 주문 주문상태 취소 변경
-		
-		주문서 주문서상태 취소 변경
+				주문서 주문서상태 취소 변경
+				
+				나머지 모든 주문들 주문상태 취소로 변경
 
 
 	 */
@@ -91,90 +91,136 @@ public class GoodsOrderService {
 		
 		int resultRow = 0;
 		
+		// ================ 주문 취소 시작 ================
 		
+		// cancel_history 주문 이력 추가
+		goodsOrderMapper.cInsertCancelHistory(paramMap);
+		
+		// 취소 전 결제 했었던 주문서 가격
+		int orderSheetPrice = goodsOrderMapper.cSelectOrderSheetPrice(paramMap);
+		
+		// paramMap 에 orderSheetPrice 추가
+		paramMap.put("orderSheetPrice", orderSheetPrice);
+		
+		// 주문서 가격(결제 가격) pay_save 이력 추가
+		goodsOrderMapper.cInsertPaySaveHistory(paramMap);
+		
+		// 주문서 가격(결제 가격) total_use_pay 복구
+		goodsOrderMapper.cUpdateTotalUsePayRewind(paramMap);
+		
+		// customer 테이블 pay 환급
+		goodsOrderMapper.cUpdateCustomerPayRewind(paramMap);
+		
+		// 주문에 사용한 총 포인트
+		int point = goodsOrderMapper.cSelectSumPointOrderCancel(paramMap);
+		
+		log.info(TeamColor.CYAN + point + " <-- point, orderCancel");
+		
+		if(point > 0) {
+			// 주문에 사용한 포인트가 있다면
+			
+			// paramMap에 포인트 저장
+			paramMap.put("point", point);
+			
+			// 사용 총 포인트가 0보다 크다면 point_save 이력 추가
+			goodsOrderMapper.cInsertPointSaveHistory(paramMap);
+			
+			// 사용 총 포인트가 0보다 크다면 customer 테이블 point 환급
+			goodsOrderMapper.cUpdateCustomerPointRewind(paramMap);
+			
+		}
+		
+		
+		
+		// =================================================================
 		
 		// 주문 취소하기 전 결제 완료 상태인 주문 갯수
 		int count = goodsOrderMapper.cSelectCountOrderPayment(paramMap);
 		
 		if(count > 1) {
 			// 취소 후 결제 완료 주문이 하나라도 남는 경우
-
-			
-			// ================ 주문 전체 취소 시작 ================
-			
-			
-			// cancel_history 주문 이력 추가
-			goodsOrderMapper.cInsertCancelHistory(paramMap);
-			
-			// 주문서 가격(결제 가격) pay_save 이력 추가
-			goodsOrderMapper.cInsertPaySaveHistory(paramMap);
-			
-			// 주문서 가격(결제 가격) total_use_pay 복구
-			goodsOrderMapper.cUpdateTotalUsePayRewind(paramMap);
-			
-			// 주문에 사용한 총 포인트
-			int point = goodsOrderMapper.cSelectSumPointOrderCancel(paramMap);
-			
-			log.info(TeamColor.CYAN + point + " <-- point, orderCancel");
-			
-			if(point > 0) {
-				
-				// 사용 총 포인트가 0보다 크다면 point_save 이력 추가
-				paramMap.put("point", point);
-				goodsOrderMapper.cInsertPointSaveHistory(paramMap);
-				
-				
-			}
 			
 			// 해당 주문 주문상태 취소로 변경
 			goodsOrderMapper.cUpdateOrderStateCancel(paramMap);
 			
-			
-			// 주문 중 결제완료인 goods_option_no, quantity 저장
+			// 주문 중 결제완료인 goods_option_no, quantity, price 저장
 			ArrayList<HashMap<String, Object>> optionList = goodsOrderMapper.cSelectOptionNoQuantityPrice(paramMap);
 			
 			log.info(TeamColor.CYAN + optionList.toString() + " <-- optionList.toString()");
 			
 			
 			// 주문서 company_id, customer_address_no 저장
-			HashMap<String, Object> sheetHashMap = goodsOrderMapper.cSelectCompanyIdCustomerAddress(paramMap);
+			HashMap<String, Object> newSheetMap = goodsOrderMapper.cSelectCompanyIdCustomerAddress(paramMap);
 			
-			log.info(TeamColor.CYAN + sheetHashMap.get("companyId") + " <-- sheetHashMap companyId");
-			log.info(TeamColor.CYAN + sheetHashMap.get("customerAddressNo") + " <-- sheetHashMap customerAddressNo");
+			// customerId 저장
+			newSheetMap.put("customerId", (String) paramMap.get("customerId"));
+			
+			log.info(TeamColor.CYAN + newSheetMap.get("companyId") + " <-- sheetHashMap companyId");
+			log.info(TeamColor.CYAN + newSheetMap.get("customerAddressNo") + " <-- sheetHashMap customerAddressNo");
 			
 			// 주문서 주문상태 취소로 변경
 			goodsOrderMapper.cUpdateOrderSheetStateCancel(paramMap);
 			
+			// 나머지 모든 주문들 주문상태 취소로 변경
+			goodsOrderMapper.cUpdateGoodsOrderStateCancel(paramMap);
 			
 			
 			
-			// ================ 주문 전체 취소 끝 ================
+			// ================ 취소 항목 제외 재주문 시작 ================
 			
 			
 			
-			// ================ 취소 제외 재주문 시작 ================
+			// 새로운 주문서 생성(새 주문서 가격은 0) 후 order_sheet_no 추출
+			goodsOrderMapper.cInsertNewOrderSheet(newSheetMap);
+			
+			log.info(TeamColor.CYAN + newSheetMap.get("newOrderSheetNo") + " <-- newOrderSheetNo");
+
+			// 취소한 주문 제외하고 나머지 주문들 재구매(서비스에서 반복문 돌림)
+			for(HashMap<String, Object> option : optionList) {
+				
+				// newOrderSheet 저장
+				option.put("newOrderSheetNo", (int) newSheetMap.get("newOrderSheetNo"));
+				
+				// 나머지 주문들 생성
+				goodsOrderMapper.cInsertNewGoodsOrder(option);
+				
+			}
+			
+			// 새로운 주문서 총 가격(등급 할인만 적용)
+			int newOrderSheetPrice = goodsOrderMapper.cSelectNewOrderSheetPrice(newSheetMap);
+			
+			log.info(TeamColor.CYAN + newOrderSheetPrice + " <-- newOrderSheetPrice");
+			
+			// newSheetMap 에 newOrderSheetPrice 추가
+			newSheetMap.put("newOrderSheetPrice", newOrderSheetPrice);
+			
+			log.info(TeamColor.CYAN + newSheetMap.toString() + " <-- newSheetMap.toString()");
+			
+			// 새로운 주문서 가격 수정
+			goodsOrderMapper.cUpdateNewOrderSheetPrice(newSheetMap);
+			
+			// pay_use_history 이력 추가
+			goodsOrderMapper.cInsertPayUseHistory(newSheetMap);
+			
+			// customer 테이블 새 주문서 가격으로 pay 차감
+			goodsOrderMapper.cUpdateCustomerPayBuy(newSheetMap);
+			
+			// total_use_pay 재구매 내용 적용
+			goodsOrderMapper.cUpdateTotalUsePayBuy(newSheetMap);
 			
 			
 			
+			// ================ 취소 항목 제외 재주문 끝 ================
 			
 			
 			
+		} else if(count == 1) {
 			
+			// 주문서 주문상태 취소로 변경
+			goodsOrderMapper.cUpdateOrderSheetStateCancel(paramMap);
 			
-			
-			
-			
-			
-			
-			
-			
-			// ================ 취소 제외 재주문 끝 ================
-			
-			
-			
-		} else {
-			
-			
+			// 나머지 모든 주문들 주문상태 취소로 변경
+			goodsOrderMapper.cUpdateGoodsOrderStateCancel(paramMap);
 			
 			
 		}
